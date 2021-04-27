c3b848f392f54a8bdbca665411de48555bd48836
Marina Dariy <dariy.mvs@gmail.com>
2021-04-27 11:39:44 +0300

    refresh 100% coverage

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,20 +1,4 @@
-import { loadUser } from '../user';
-import { httpGet } from '../http';
 import getStringHealth from '../app';
-
-jest.mock('../http');
-
-beforeEach(() => {
-  jest.resetAllMocks();
-});
-
-test('should call loadUser once', () => {
-  httpGet.mockReturnValue(JSON.stringify({}));
-
-  const response = loadUser(1);
-  expect(response).toEqual({});
-  expect(httpGet).toBeCalledWith('http://server:8080/users/1');
-});
 
 test('getStringHealth', () => {
   const result = getStringHealth({ name: 'elf', health: 90 });
